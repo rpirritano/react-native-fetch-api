b25c5f4984a54e39b45e0c7f370447da19936994
Robert Pirritano <rpirritano@gmail.com>
2018-08-29 15:03:46 -0700

    completed fetching from api

--- App.js
+++ App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, ScrollView, ActivityIndicator, Image, Dimensions } from 'react-native'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      productImages: [],
+      fetching: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ fetching: true })
+    fetch('https://hplussport.com/api/products.php')
+      .then(response => response.json())
+      .then(products => products.map(product => product.image))
+      .then(productImages => this.setState({
+        productImages,
+        fetching: false
+      }))
+      .catch(err => console.error('error fetching products', err))
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <ScrollView horizontal={true}>
+        <ActivityIndicator size="large"
+          style={styles.spinner}
+          animating={this.state.fetching} />
+          {this.state.productImages.map((uri, i) => (
+            <Image style={styles.thumb} key={i} source={{ uri }} />
+          ))}
+      </ScrollView>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  spinner: {
+    position: 'absolute',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
   },
-});
+  thumb: {
+    width: 375,
+    resizeMode: 'cover'
+  }
+})
